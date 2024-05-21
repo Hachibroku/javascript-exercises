53aebe2873f90f44183d682902cdc905b5aee6a8
Murphey Osmundson <mosmundson88@gmail.com>
2024-05-20 22:25:55 -0400

    test 4 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    // search array of instances of removalCandidate
+    // return index and length of removalCandidate(s)
+    // result = array.splice(index, length)
+    // return result
+    return array.filter(element => !args.includes(element))
 };
 
 // Do not edit below this line
